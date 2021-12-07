c85ec91ecf20195b49258b830ef748421b53057d
Alexander <v0xt.alex@gmail.com>
2021-12-07 13:34:45 +0300

    oh you are from england

--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -8,35 +8,35 @@ interface IERC20 {
     function totalSupply() external view returns (uint256);
 
     /** @notice Returns amount of tokens owned by `account`.
-     * @param account Address of the token holder.
+     * @param account The address of the token holder.
      * @return The amount of tokens in uint.
      */
     function balanceOf(address account) external view returns (uint256);
 
     /** @notice Transfers `amount` of tokens to specified address.
-     * @param to Address of recipient.
+     * @param to The address of recipient.
      * @param amount The amount of tokens to transfer.
      * @return True if transfer was successfull.
      */
     function transfer(address to, uint256 amount) external returns (bool);
 
     /** @notice Returns the number of tokens approved by an `owner` to a `spender`.
-     * @param owner Address of an owner of approved tokens.
-     * @param spender Approved address.
+     * @param owner Address of the owner of approved tokens.
+     * @param spender The approved address.
      * @return The amount of tokens in uint.
      */
     function allowance(address owner, address spender) external view returns (uint256);
 
     /** @notice Approves `spender` to use `amount` of function caller tokens.
-     * @param spender Address of recipient.
+     * @param spender The address of recipient.
      * @param amount The amount of tokens to approve.
      * @return The amount of tokens in uint.
      */
     function approve(address spender, uint256 amount) external returns (bool);
 
     /** @notice Allows a spender to spend an allowance.
-     * @param sender Address of spender.
-     * @param recipient Address of recipient.
+     * @param sender The address of spender.
+     * @param recipient The address of recipient.
      * @param amount The amount of tokens to transfer.
      * @return True if transfer was successfull.
      */
@@ -46,16 +46,16 @@ interface IERC20 {
         uint256 amount
     ) external returns (bool);
 
-    /** @notice This event is emitted when a token transfer occurs.
+    /** @notice Emitted when a token transfer occurs.
      * @param from Sender`s address.
      * @param to Recipient`s address.
      * @param value The amount of transferred tokens.
      */
     event Transfer(address indexed from, address indexed to, uint256 value);
 
-    /** @notice This event is emitted when a token approval occurs.
+    /** @notice Emitted when a token approval occurs.
      * @param owner The source account.
-     * @param spender Address of spender.
+     * @param spender The address of spender.
      * @param value The amount of tokens.
      */
     event Approval(address indexed owner, address indexed spender, uint256 value);
