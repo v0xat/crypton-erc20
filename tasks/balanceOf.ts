bae9fffe92508a9eec465c9cb1e2c19292125cda
Alexander <v0xt.alex@gmail.com>
2021-12-08 13:09:32 +0300

    Update tasks

--- tasks/balanceOf.ts
+++ tasks/balanceOf.ts
@@ -1,9 +1,8 @@
 import fs from 'fs';
 import { task } from "hardhat/config";
-import { ethers } from "ethers";
 import dotenv from 'dotenv';
 
-task("token-balanceOf", "Prints an account's token balance")
+task("token-balance", "Prints an account's token balance")
   .addParam("account", "The account's address")
   .setAction(async (taskArgs, hre) => {
     const network = hre.network.name;
@@ -18,5 +17,6 @@ task("token-balanceOf", "Prints an account's token balance")
     );
     
     const balance = await cryptonToken.balanceOf(taskArgs.account);
-    console.log(`${taskArgs.account} account balance is ${ethers.utils.formatUnits(balance, 18)} tokens`);
+    const format = hre.ethers.utils.formatUnits(balance, process.env.CRYPTON_TOKEN_DECIMALS);
+    console.log(`${taskArgs.account} account balance is ${format} tokens`);
   });
--- tasks/index.ts
+++ tasks/index.ts
@@ -1,2 +1,3 @@
 import "./accounts";
 import "./balanceOf";
+import "./transfer";
